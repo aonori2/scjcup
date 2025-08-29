7ef887806c0e4554330821a89197a07b2f18c098
Aoki Noriyasu <Aoki Noriyasu>
2025-08-30 00:54:50 +0900

    first commit

---
+++ wp-content/themes/jstork_custom/page-match.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+
+<div class="mian-image-area">
+    <img class="main-img" src="<?=get_stylesheet_directory_uri(); ?>/img/S__2285783.jpg">
+</div>
+<main class="container py-5">
+<section>
+    <h2 class="text-center font-bold title">対戦表</h2>
+    <!--
+    <h3 style="margin-top:2em; text-align:center;">グループA</h3>
+    <div style="text-align:center; width:100%;"><img src="<?=home_url();?>/wp-content/uploads/2023/07/1e512bcc6c31f3456d3022dddc906fff.jpg"></div>
+    <h3 style="margin-top:2em; text-align:center;">グループB</h3>
+    <div style="text-align:center; width:100%;"><img src="<?=home_url();?>/wp-content/uploads/2023/07/bf31429139b4f5c489608ae9393adff7.jpg"></div>
+    <h3 style="margin-top:2em; text-align:center;">グループC</h3>
+    <div style="text-align:center; width:100%;"><img src="<?=home_url();?>/wp-content/uploads/2023/07/78397140bf66dba0f67e788220933dab.jpg"></div>
+    -->
+    <h3 style="margin-top:2em; text-align:center;">プレミアリーグ</h3>
+    <h4 style="text-align:center">7月16日 レッズランド</h2>
+    <div style="text-align:center; width:100%;"><img src="<?=get_stylesheet_directory_uri(); ?>/img/プレミアリーグ.jpg"></div>
+
+    <h3 style="margin-top:2em; text-align:center;">ゴールドリーグ</h3>
+    <h4 style="text-align:center">7月16日	レッズハートフルフィールド駒場</h2>
+    <div style="text-align:center; width:100%;"><img src="<?=get_stylesheet_directory_uri(); ?>/img/ゴールドリーグ.jpg"></div>
+
+    <h3 style="margin-top:2em; text-align:center;">シルバーリーグ</h3>
+    <h4 style="text-align:center">7月16日	さいたま市堀崎公園グラウンド</h2>
+    <div style="text-align:center; width:100%;"><img src="<?=get_stylesheet_directory_uri(); ?>/img/シルバーリーグ.jpg"></div>
+</section>
+</main>
+
+<?php get_footer(); ?>
